150cae87268f130521dff2228266d3f4131e67de
sbx_user1051 <sbx_user1051@169.254.8.53>
2020-10-31 10:26:40 +0000

    Sat Oct 31 10:26:40 UTC 2020

---
+++ src/BeFaster.App/Solutions/CHK/ProductOffer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeFaster.App.Solutions.CHK
+{
+    public class ProductOffer
+    {
+        public string SKU { get; set; }
+        public int Quantity { get; set; }
+        public int Value { get; set; }
+    }
+}
