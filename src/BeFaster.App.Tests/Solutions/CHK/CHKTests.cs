28703353374afae4c16c90a70c1492ce1c54570d
sbx_user1051 <sbx_user1051@169.254.8.53>
2020-10-31 10:29:40 +0000

    Sat Oct 31 10:29:40 UTC 2020

--- src/BeFaster.App.Tests/Solutions/CHK/CHKTests.cs
+++ src/BeFaster.App.Tests/Solutions/CHK/CHKTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BeFaster.App.Solutions.CHK;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,25 @@ using System.Threading.Tasks;
 
 namespace BeFaster.App.Tests.Solutions.CHK
 {
-    class CHKTests
+    public class CHKTests
     {
+        private List<Product> _products;
+        private List<SpecialOffer> _discounts;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _products = new List<Product>()
+            {
+                new Product{ SKU = "A", Price = 50},
+                new Product{ SKU = "B", Price = 30},
+                new Product{ SKU = "C", Price = 20},
+                new Product{ SKU = "D", Price = 15}
+            };
+
+
+
+        }
     }
 }
+
--- src/BeFaster.App/Solutions/CHK/ProductOffer.cs
+++ src/BeFaster.App/Solutions/CHK/ProductOffer.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace BeFaster.App.Solutions.CHK
 {
-    public class ProductOffer
+    public class SpecialOffer
     {
         public string SKU { get; set; }
         public int Quantity { get; set; }
         public int Value { get; set; }
     }
 }
+
